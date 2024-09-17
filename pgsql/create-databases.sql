a1a7c2d136cca1201c0c06bb072d37453a8a89f3
Oleg Shabashov <oleg.shabashov@gmail.com>
2024-09-17 10:30:23 +0700

    Fix creating databases

--- pgsql/create-databases.sql
+++ pgsql/create-databases.sql
@@ -1,11 +1,11 @@
-SELECT 'CREATE DATABASE client_admin'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'client_admin')\gexec;
+SELECT 'CREATE DATABASE shopify_admin'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'shopify_admin')\gexec;
 
-SELECT 'CREATE DATABASE client_admin_testing'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'client_admin_testing')\gexec;
+SELECT 'CREATE DATABASE shopify_admin_testing'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'shopify_admin_testing')\gexec;
 
-SELECT 'CREATE DATABASE search_or_die'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'search_or_die')\gexec;
+SELECT 'CREATE DATABASE cassie'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cassie')\gexec;
 
-SELECT 'CREATE DATABASE search_or_die_testing'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'search_or_die_testing')\gexec;
+SELECT 'CREATE DATABASE cassie_testing'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cassie_testing')\gexec;
