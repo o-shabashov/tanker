c5d9366a3e8e62226b564a33be8a81745907766d
Oleg Shabashov <oleg.shabashov@gmail.com>
2024-10-09 13:16:40 +0700

    Fix creating databases

--- pgsql/create-databases.sql
+++ pgsql/create-databases.sql
@@ -1,11 +1,6 @@
-SELECT 'CREATE DATABASE shopify_admin'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'shopify_admin');
+-- Already created in docker-compose.yml by default, see .env
+-- CREATE DATABASE cassie;
 
-SELECT 'CREATE DATABASE shopify_admin_testing'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'shopify_admin_testing');
-
-SELECT 'CREATE DATABASE cassie'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cassie');
-
-SELECT 'CREATE DATABASE cassie_testing'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cassie_testing');
+CREATE DATABASE cassie_testing;
+CREATE DATABASE shopify_admin;
+CREATE DATABASE shopify_admin_testing;
